570ff452732d72292ab7925e4ec56aeccbe7ce75
AndriiDorohov <wedpositive@gmail.com>
2025-01-13 22:46:56 +0100

    Add tasks for Chapter 1: Primitive data types

---
+++ task_02.py
@@ -0,0 +1,10 @@
+# Task 2: Floating Point Operations
+# Problem:
+# 1. Create a float variable radius with a value of 7.5.
+# 2. Calculate the area of a circle with this radius using the formula area = π * r^2.
+#  - Use 3.14159 for π.
+# 3. Print the area of the circle.
+
+radius = 7.5
+area = 3.14159 * radius**2
+print("\nTASK 2\nThe area of the circle is equal to:", area)
